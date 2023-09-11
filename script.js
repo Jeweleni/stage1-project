39fdeaa8cb9c88919939354ddd9058ebea5c6845
Eni Momoreoluwa Jewel <109070607+Jeweleni@users.noreply.github.com>
2023-09-11 20:26:18 +0100

    final commit

--- script.js
+++ script.js
@@ -1,17 +1,57 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const realTimeDataElement = document.querySelector('[data-testid="real-time-data"]');
+    const slackUserNameElement = document.querySelector('[data-testid="slackUserName"]');
+    const slackDisplayImageElement = document.querySelector('[data-testid="slackDisplayImage"]');
+    const currentDayOfTheWeekElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+    const currentUTCTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
+    const myTrackElement = document.querySelector('[data-testid="myTrack"]');
+    const githubURLElement = document.querySelector('[data-testid="githubURL"]');
+    // const realTimeDataElement = document.querySelector('[data-testid="real-time-data"]');
+    
+    function updateSlackInfo() {
+        const slackName = "Jewel Eni";
+        const slackImageURL = "https://ca.slack-edge.com/T05FFAA91JP-U05RQNWB79P-661f25735c19-512";
 
-    // Function to update the real-time data
-    function updateRealTimeData() {
-        const currentDate = new Date();
-        const formattedDate = currentDate.toLocaleString();
-
-        realTimeDataElement.textContent = `Current Date and Time: ${formattedDate}`;
+        slackUserNameElement.textContent = slackName;
+        slackDisplayImageElement.innerHTML = `<img src="${slackImageURL}" alt="${slackName}" />`;
     }
 
-    // Initial update
-    updateRealTimeData();
+    function updateDayOfTheWeek() {
+        const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        const currentDate = new Date();
+        const dayOfWeek = daysOfWeek[currentDate.getUTCDay()];
 
-    // Update the real-time data every second (1000 milliseconds)
-    setInterval(updateRealTimeData, 1000);
+        currentDayOfTheWeekElement.textContent = `Current Day of the Week: ${dayOfWeek}`;
+    }
+
+    function updateUTCTime() {
+        const currentUTC = new Date().getTime();
+
+        currentUTCTimeElement.textContent = `Current UTC Time: ${currentUTC} milliseconds`;
+    }
+
+    // function updateRealTimeData() {
+    //     const currentrealTimeData = new Date();
+        
+    //     currentrealTimeDataElement.textContent =  `Current Date and Time: ${formattedDate}`;
+    // }
+       
+
+    function updateTrack() {
+        const track = "Frontend Development";
+
+        myTrackElement.textContent = `My Track: ${track}`;
+    }
+
+    function updateGitHubURL() {
+        const githubURL = "https://github.com/Jeweleni/stage1-project";
+
+        githubURLElement.innerHTML = `<a href="${githubURL}" target="_blank">GitHub Repository</a>`;
+    }
+
+    updateSlackInfo();
+    updateDayOfTheWeek();
+    updateUTCTime();
+    // updateRealTimeData();
+    updateTrack();
+    updateGitHubURL();
 });
